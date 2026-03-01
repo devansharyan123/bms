d2005e7fea0dccfd6ecf2d68534bda9fca21cc9a
Devansh Aryan <devansh123aryan@gmail.com>
2026-03-01 19:17:09 +0530

    feat: add HTTP and WebSocket servers for user signup and connection handling
    - Implemented an HTTP server with a signup endpoint that creates a user in the database.
    - Added a WebSocket server that sends a welcome message upon connection and creates a user with random credentials.

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express"
+import {client} from "@repo/db/client"
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/" , (req , res) => {
+    res.send("Hi there")
+})
+
+app.post("/signup" , async (req , res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+  
+ const user = await client.user.create({
+    data : {
+        username,
+        password
+    }
+  })
+  res.json({
+    message : "signUp successfull",
+    id : user.id
+  })
+
+})
+
+app.listen(3000);
